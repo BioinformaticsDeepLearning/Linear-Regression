83370f3802186fe9050d9dd0398d859e8e6c685b
Alisha Parveen <49845542+aparveen-prog@users.noreply.github.com>
2020-07-22 22:11:27 +0200

    Update Linear-regression.R

--- Linear-regression.R
+++ Linear-regression.R
@@ -1,19 +1,19 @@
-Linear Regression is widely used in statistical analysis between two variable. It is used to establish the relationship between 2 variable. 
-One variable is called predictor variable, which is generated from experimentation whereas other variable called responsive variable which is dependent on predictor variable.
-Simple equation for Linear regression is:
-                y = ax+b
-            Where,
-                  y is the responsive variable
-                  x is the predictor
-                  a and b is the coefficient
+#Linear Regression is widely used in statistical analysis between two variable. It is used to establish the relationship between 2 variable. 
+# One variable is called predictor variable, which is generated from experimentation whereas other variable called responsive variable which is dependent on predictor variable.
+#Simple equation for Linear regression is:
+#                y = ax+b
+#           Where,
+#                  y is the responsive variable
+#                  x is the predictor
+#                  a and b is the coefficient
      
 #In order to create a simple linear regression model#
-  1. Input dataset (two variable)
-  2. Create a relationship between the 2 variable using lm() function, it is inbuilt R function.
-  3. Find the coefficient of the model and generate a mathematical equations from these coefficient 
-  4. Get the summary of the model by predicting the average error.
-  5. Go with the prediction of the new data by using predict() function in R.
-  6. Go for the visualization
+#  1. Input dataset (two variable)
+#  2. Create a relationship between the 2 variable using lm() function, it is inbuilt R function.
+#  3. Find the coefficient of the model and generate a mathematical equations from these coefficient 
+#  4. Get the summary of the model by predicting the average error.
+#  5. Go with the prediction of the new data by using predict() function in R.
+#  6. Go for the visualization
 
 #Input variables#
   x <- c(151, 174, 138, 186, 128, 136, 179, 163, 152, 131)
